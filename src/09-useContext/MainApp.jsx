7ad9ba256938b4fc278322b951c19ec170e87430
luislealdev <luisrrleal@gmail.com>
2022-06-30 18:42:15 -0500

    Start using react router and links, also create a navbar

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes, Navigate } from "react-router-dom"
+import { AboutPage } from "./AboutPage"
+import { HomePage } from "./HomePage"
+import { LoginPage } from "./LoginPage"
+import { Navbar } from "./Navbar"
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>MainApp</h1>
+        <Navbar/>
+        <hr/>
+
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/*" element={<Navigate to={"/"}/>} />
+      </Routes>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
 // import { CounterApp } from './01-useState/CounterApp'
@@ -15,11 +16,14 @@ import './index.css'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer'
-import { TodosApp } from "./08-useReducer/TodosApp";
+// import { TodosApp } from "./08-useReducer/TodosApp";
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <React.StrictMode>
-  <TodosApp/>
+  <MainApp/>
   </React.StrictMode>
+  </BrowserRouter>
 )
 
