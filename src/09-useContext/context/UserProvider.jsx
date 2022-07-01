2c611f495a805bb76771c5d76732e0ef7d855281
luislealdev <luisrrleal@gmail.com>
2022-06-30 19:23:28 -0500

    create context and contextProvider

---
+++ src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,15 @@
+import { UserContext } from "./UserContext";
+
+const user = {
+    id: 1,
+    name: 'Luis Leal',
+    email: 'luisleal@gmail.com',
+}
+
+export const UserProvider = ({children}) =>{
+    return(
+        <UserContext.Provider value={{hola:'Mundo', user}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
