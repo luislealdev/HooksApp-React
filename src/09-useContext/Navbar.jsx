150bf3a952590874d0590fd27be6db4d452bae8a
luislealdev <luisrrleal@gmail.com>
2022-06-30 19:02:19 -0500

    create a navbar component and introduce the navlinks

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,11 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
+      <nav className="navbar navbar-expand-lg bg-light rounded-3">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <NavLink
+                to="/"
+                className={`nav-link ${({ isActive }) => {
+                  return isActive ? "active" : "";
+                }}`}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                className={`nav-link ${({ isActive }) => {
+                  return isActive ? "active" : "";
+                }}`}
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/login"
+                className={`nav-link ${({ isActive }) => {
+                  return isActive ? "active" : "";
+                }}`}
+              >
+                Login
+              </NavLink>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
