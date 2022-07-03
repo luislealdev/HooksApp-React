2549c8628dfa47f5ac23b5c556e58bb7f15fc8be
luislealdev <luisrrleal@gmail.com>
2022-07-02 22:31:15 -0500

    complete the module but it has an error, working on it

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,30 @@
+
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+
+
 export const LoginPage = () => {
-  return (
-    <>
-        <h1>LoginPage</h1>
-        <hr/>
-    </>
-  )
-}
+
+    const { user, setUser } = useContext( UserContext );
+    
+    return (
+      <>
+          <h1>LoginPage</h1>
+          <hr />
+
+          <pre>
+            { JSON.stringify( user, null, 3 ) }
+          </pre>
+
+
+          <button 
+            className="btn btn-primary"
+            onClick={ () => setUser({ id: 123, name: 'Juan', email: 'juan@google.com' })  }
+          >
+            Establecer usuario
+          </button>
+
+      </>
+    )
+  }
+  
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,13 @@
-import { UserContext } from "./UserContext";
+import { useState } from "react"
+import { UserContext } from "./UserContext"
 
-const user = {
-    id: 1,
-    name: 'Luis Leal',
-    email: 'luisleal@gmail.com',
-}
+export const UserProvider = ({ children }) => {
 
-export const UserProvider = ({children}) =>{
-    return(
-        <UserContext.Provider value={{hola:'Mundo', user}}>
-            {children}
+    const [user, setUser] = useState();
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            { children }
         </UserContext.Provider>
     )
 }
