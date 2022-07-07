2e39743e42e9afee25fa1edf5f28f7ee4e9aef96
luislealdev <luisrrleal@gmail.com>
2022-07-07 18:38:01 -0500

    Tests in <HomePage/>

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
     return (
       <>
           <h1>HomePage</h1>
-          <pre>
+          <pre aria-label="pre">
             { JSON.stringify( user, null, 3 ) }
           </pre>
 
---
+++ tests/09-useContext/HomePage.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { UserContext } from "../../src/09-useContext/context/UserContext";
+import { HomePage } from "../../src/09-useContext/HomePage";
+
+describe("Tests in <HomePage/> using useContext", () => {
+  test("should show the pre message (null) when the user is not defined", () => {
+    const user = null;
+
+    render(
+      <UserContext.Provider value={{ user }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+
+    const pre = screen.getByLabelText("pre");
+    console.log(pre.innerHTML);
+    expect(pre.innerHTML).toBe("null");
+  });
+
+  test("should show the user when it is defined", () => {
+
+    const user = {
+        id:1,
+        name:'Luis'
+    };
+
+    render(
+      <UserContext.Provider value={{ user }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+
+    const screenUser = screen.getByLabelText("pre");
+    expect(screenUser.innerHTML).toContain(user.name);
+    expect(screenUser.innerHTML).toContain(user.id.toString());
+  });
+});
